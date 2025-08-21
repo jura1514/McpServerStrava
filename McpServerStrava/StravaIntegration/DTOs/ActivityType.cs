ef263eba5f9277fe82d38233bffdb0de41044945
Georgij Nebudrov <gene@danskebank.lt>
2025-08-21 13:18:12 +0300

    accidentally committed previously so removing

--- McpServerStrava/StravaIntegration/DTOs/ActivityType.cs
+++
@@ -1,8 +0,0 @@
-namespace StravaIntegration.DTOs;
-
-public enum ActivityType
-{
-    Run = 1,
-    Hike = 2,
-    WeightTraining = 3
-}
--- McpServerStrava/StravaIntegration/DTOs/RefreshTokenRequest.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace StravaIntegration.DTOs;
-
-public record RefreshTokenRequest(
-    [Required(ErrorMessage = "Refresh token is required")]
-    [MinLength(1, ErrorMessage = "Refresh token cannot be empty")]
-    string RefreshToken
-);
